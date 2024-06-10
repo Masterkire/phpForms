d5904c7d660a2590d7bccbc61b131e574e815832
master work <davidmasterkire@gmail.com>
2024-06-10 09:52:17 -0600

    terminando archivo de validando datos

--- validando-datos/server.php
+++ validando-datos/server.php
@@ -1,14 +1,14 @@
 <?php
 
-$is_float = filter_var("esto no es un float", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND);
+$is_float = filter_var("0.9", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND);
 
-$is_int = filter_var("esto no es un numero", FILTER_VALIDATE_INT);
+$is_int = filter_var("45", FILTER_VALIDATE_INT);
 
-$is_ip = filter_var("0.0.0", FILTER_VALIDATE_IP);
+$is_ip = filter_var("127.0.0.1", FILTER_VALIDATE_IP);
 
-$is_url = filter_var("platzi.com", FILTER_VALIDATE_URL);
+$is_url = filter_var("https://platzi.com", FILTER_VALIDATE_URL);
 
-$is_email = filter_var("Esto no es un email", FILTER_VALIDATE_EMAIL);
+$is_email = filter_var("correo@correo.com", FILTER_VALIDATE_EMAIL);
 
 echo "<pre>";
 var_dump($is_float);
